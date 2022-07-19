c4ebc77419b4fe62d9ef633452e5d9359a8e439e
Renato Spakauskas <renatospaka@gmail.com>
2022-07-19 12:17:32 -0300

    feat: :white_check_mark: singleton
    an exemple of unit test using singleton

--- singleton/singCapitals.go
+++ singleton/singCapitals.go
@@ -2,12 +2,31 @@ package singleton
 
 import "sync"
 
+type Database interface {
+	GetPopulation(name string) int
+}
+
 type singletonDatabase struct {
 	capitals map[string]int
 }
 
 func (db *singletonDatabase) GetPopulation(name string) int {
 	return db.capitals[name]
+}
+
+type DummyDatabase struct {
+	dummyData map[string]int
+}
+
+func (d *DummyDatabase) GetPopulation(name string) int {
+	if len(d.dummyData) == 0 {
+		d.dummyData = map[string]int {
+			"alpha": 1,
+			"beta": 2,
+			"gamma": 3,
+		}
+	}
+	return d.dummyData[name]
 }
 
 var once sync.Once	// thread safety
@@ -24,4 +43,20 @@ func GetSingletonDatabase() *singletonDatabase {
 		instance = &db
 	})
 	return instance
+}
+
+func GetTotalPopulation(cities []string) int {
+	result := 0
+	for _, city := range cities {
+		result += GetSingletonDatabase().GetPopulation(city)
+	} //DIP
+	return result
+}
+
+func GetTotalPopulationEx(db Database, cities []string) int {
+	result := 0
+	for _, city := range cities {
+		result += db.GetPopulation(city)
+	} 
+	return result
 }
--- tests/testSingleton.go
+++ tests/testSingleton.go
@@ -10,4 +10,14 @@ func TestSingleton() {
 	db := singleton.GetSingletonDatabase()
 	pop := db.GetPopulation("Seoul")
 	fmt.Printf("SINGLETON => Population of %s = %d\n", "Seoul", pop)
+
+	//UT
+	cities := []string{"Seoul", "Mexico City"}
+	tp := singleton.GetTotalPopulation(cities)
+	fmt.Printf("SINGLETON => Population of %s = %d => OK=(%t)\n", cities, tp, tp == (17500000 + 17400000))
+
+	//UT + DIP
+	cities = []string{"alpha", "gamma"}
+	tp = singleton.GetTotalPopulationEx(&singleton.DummyDatabase{}, cities)
+	fmt.Printf("SINGLETON => Population of %s = %d => OK=(%t)\n", cities, tp, tp == 4)
 }
